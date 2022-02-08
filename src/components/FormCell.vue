ee088c186258dbcb6d06fad006d08c5619fe3e40
steven <steven_ko@paradise-soft.com.tw>
2022-02-08 17:38:16 +0800

    feat(FormCell): add 多功能input框

---
+++ src/components/FormCell.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <input v-if="formType === 'text'" type="text" v-model="newValue">
+
+    <div v-if="formType === 'radio'">
+      <input type="radio" id="boy" value="男" v-model="newValue">
+      <label for="boy">男</label>
+      <br>
+      <input type="radio" id="girl" value="女" v-model="newValue">
+      <label for="girl">女</label>
+    </div>
+
+    <div v-if="formType === 'select'">
+      <select v-model="newValue">
+        <option disabled value="">请选择</option>
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="C">C</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormCell',
+  model: {
+    prop: 'value',
+    event: 'update:value'
+  },
+  props: {
+    formType: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+  },
+  computed: {
+    newValue: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('update:value', val)
+      }
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -7,10 +7,35 @@
     <button class="theme-default-btn">默认</button>
     <button class="theme-primary-btn">主要</button>
     <button class="theme-info-btn">提示</button>
+    <br />
+    <FormCell v-model="form.name" />
+    <br />
+    <FormCell v-model="form.gender" formType="radio" />
+    <br />
+    <FormCell v-model="form.score" formType="select" />
+
+    <div>
+      <p>姓名: {{ form.name }}</p>
+      <p>性別: {{ form.gender }}</p>
+      <p>分數: {{ form.score }}級</p>
+    </div>
   </div>
 </template>
 <script>
+import FormCell from '@/components/FormCell'
 export default {
+  components: {
+    FormCell
+  },
+  data() {
+    return {
+      form: {
+        name: '',
+        gender: '',
+        score: ''
+      }
+    }
+  },
   methods: {
     blueTheme() {
       this.$store.dispatch("themeChange", "blue");
